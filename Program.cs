f7c6481430785ef913577bbb64e2ff4cb05e25c2
João Eduardo <joaoeduardoj920@gmail.com>
2024-01-28 17:04:07 -0300

    feat: adding cor for pecas

--- Program.cs
+++ Program.cs
@@ -11,7 +11,9 @@ try
 
   tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preto), new Posicao(0, 0));
   tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preto), new Posicao(1, 3));
-  tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preto), new Posicao(4, 3));
+  tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Branco), new Posicao(4, 3));
+  tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Branco), new Posicao(6, 3));
+  tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Branco), new Posicao(6, 2));
 
   TelaTabuleiro.Mostra_tela_do_tabuleiro(tabuleiro);
   Console.ReadLine();
--- screen/TelaTabuleiro.cs
+++ screen/TelaTabuleiro.cs
@@ -8,22 +8,43 @@ namespace Xadres.screen
     {
       for (int i = 0; i < tabuleiro.linhas; i++)
       {
-
+        Console.Write(8 - i + " ");
         for (int j = 0; j < tabuleiro.colunas; j++)
         {
+
+
+
           if (tabuleiro.Peca(i, j) == null)
           {
             Console.Write("- ");
             continue;
           }
 
-          Console.Write(tabuleiro.Peca(i, j) + " ");
+          ImprimirPeca(tabuleiro.Peca(i, j));
 
 
         }
 
         Console.WriteLine();
       }
+
+      Console.WriteLine("  a b c d e f g h");
     }
+
+    public static void ImprimirPeca(Peca peca)
+    {
+      if (peca.cor == Cor.Branco)
+      {
+        Console.Write(peca + " ");
+      }
+      else
+      {
+        ConsoleColor aux = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write(peca + " ");
+        Console.ForegroundColor = aux;
+      }
+    }
+
   }
 }

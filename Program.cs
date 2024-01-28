3fa382bfb1c79c620e0e04658707b9c999525f4a
João Eduardo <joaoeduardoj920@gmail.com>
2024-01-28 17:38:28 -0300

    feat: adding move

--- Program.cs
+++ Program.cs
@@ -3,15 +3,26 @@
 using Xadres.app;
 using Xadres.screen;
 using Xadres.tabuleiro;
-using Xadres.xadres;
 
 try
 {
 
   PartidaDeXadres partida = new PartidaDeXadres();
 
+  while (!partida.Terminada)
+  {
+    Console.Clear();
+    TelaTabuleiro.Mostra_tela_do_tabuleiro(partida.Tabuleiro);
 
-  TelaTabuleiro.Mostra_tela_do_tabuleiro(partida.Tabuleiro);
+    Console.Write("Origem: ");
+    Posicao origem = TelaTabuleiro.LerPosicaoxadres().ToPosicao();
+    Console.Write("Destino: ");
+    Posicao destino = TelaTabuleiro.LerPosicaoxadres().ToPosicao();
+
+
+    partida.execultarMovimento(origem, destino);
+  }
+
   Console.ReadLine();
 }
 catch (TabuleiroExeption error)
--- screen/TelaTabuleiro.cs
+++ screen/TelaTabuleiro.cs
@@ -1,10 +1,12 @@
 using Xadres.tabuleiro;
+using Xadres.xadres;
 
 namespace Xadres.screen
 {
   public class TelaTabuleiro
   {
     public static void Mostra_tela_do_tabuleiro(Tabuleiro tabuleiro)
+
     {
       for (int i = 0; i < tabuleiro.linhas; i++)
       {
@@ -31,6 +33,16 @@ namespace Xadres.screen
       Console.WriteLine("  a b c d e f g h");
     }
 
+    public static PosicaoXadres LerPosicaoxadres()
+    {
+      string s = Console.ReadLine();
+
+      char coluna = s[0];
+
+      int linha = int.Parse(s[1] + "");
+
+      return new PosicaoXadres(coluna, linha);
+    }
     public static void ImprimirPeca(Peca peca)
     {
       if (peca.cor == Cor.Branco)
